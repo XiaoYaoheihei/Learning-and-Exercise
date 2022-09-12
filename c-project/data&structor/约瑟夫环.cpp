186d7e6ab91e5f02d84090da153587e156d44472
XiaoYaoheihei <105696320+XiaoYaoheihei@users.noreply.github.com>
2022-09-12 14:48:32 +0800

    版本2.0
    加上了随机数等问题

--- c-project/data&structor/约瑟夫环.cpp
+++ c-project/data&structor/约瑟夫环.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 //约瑟夫环的实现
 using namespace std;
 void creat(int , int);
 typedef struct node 
 {
+	int num;
 	int data;
 	struct node* next;
 }Node;
@@ -21,36 +23,47 @@ int main() {
     return 0;
 }
 
-void creat(int n, int m) {
+void creat(int n, int a) {
+    int m = a;//刚开始的终止点
     int flag = 1;
     Node *head, *p, *r;
     head = (Node *)malloc(sizeof(Node));
+    head->num = 1;
     head->data = 1;
     head->next = NULL;
     r = head;
-
+    srand((unsigned)time(NULL));
     for(int i = 2; i <= n; i++) {
         p = (Node*)malloc(sizeof(Node));
-        p->data = i;
+        p->num = i;//指定每个人的序号
+        p->data = rand()%n + 1;//使用随机函数指定数字
         p->next = NULL;
         r->next = p;
         r = p;
     }
     r->next = head;//创建循环链表
-    //r = head;
+    cout << "第一个报数的人的序号为：" << endl;
+    //采用随机数函数随机指定一个号码number
+    int number = rand()%n + 1;
+    cout << number << endl;
+
+    for (int i = 1; i < number; i++) {
+        head = head->next;//更改头结点的位置
+    }
 
     while(flag < n) {
         for (int i = 1; i < m; i++) {
             r = head;
             head = head->next;
         }
-        cout << head->data << " ";
+        cout << head->num << ":" << head->data << endl;
+        m = head->data;
         Node *temp = head; //及时删除，防止出现内存泄漏问题
         r->next = head->next;
         head = head->next;
         free(temp);
         flag++;
     }
-    cout << head->data << endl;
+    cout << head->num << ":" << head->data << endl;
    
 }
